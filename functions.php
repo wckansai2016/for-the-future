7aad18e421ef4d631369aa003366bb32e40c22e9
travis-ci <wck2016@travis-ci.org>
2016-03-15 06:46:13 +0000

    Update from travis 5f92fde4e98c834f475e9d53cfe2a8f4fa7f0c60

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
 
-if ( ! function_exists( 'theme_env_sample_setup' ) ) :
+if ( ! function_exists( 'for_the_future_setup' ) ) :
 
-	function theme_env_sample_setup() {
+	function for_the_future_setup() {
 
-		load_theme_textdomain( 'theme-env-sample', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'for-the-future', get_template_directory() . '/languages' );
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'primary' => esc_html__( 'Primary', 'theme_env_sample' ),
+			'primary' => esc_html__( 'Primary', 'for-the-future' ),
 		) );
 
 		add_theme_support( 'html5', array(
@@ -29,31 +29,31 @@ if ( ! function_exists( 'theme_env_sample_setup' ) ) :
 			'link',
 		) );
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'theme_env_sample_custom_background_args', array(
+		add_theme_support( 'custom-background', apply_filters( 'for_the_future_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
 	}
 endif;
-add_action( 'after_setup_theme', 'theme_env_sample_setup' );
+add_action( 'after_setup_theme', 'for_the_future_setup' );
 
-function theme_env_sample_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'theme_env_sample_content_width', 640 );
+function for_the_future_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'for_the_future_content_width', 640 );
 }
 
-add_action( 'after_setup_theme', 'theme_env_sample_content_width', 0 );
+add_action( 'after_setup_theme', 'for_the_future_content_width', 0 );
 
 /**
  * Enqueue scripts and styles.
  */
-function theme_env_sample_scripts() {
+function for_the_future_scripts() {
 	$theme         = wp_get_theme();
 	$theme_version = $theme->get( 'Version' );
-	wp_enqueue_style( 'theme-env-sample-style', get_stylesheet_uri(), array(), $theme_version );
-	wp_enqueue_script( 'theme-env-sample-script', get_template_directory_uri() . '/bundle.js', array( 'jquery' ), $theme_version, true );
+	wp_enqueue_style( 'for-the-future-style', get_stylesheet_uri(), array(), $theme_version );
+	wp_enqueue_script( 'for-the-future-script', get_template_directory_uri() . '/bundle.js', array( 'jquery' ), $theme_version, true );
 }
 
-add_action( 'wp_enqueue_scripts', 'theme_env_sample_scripts' );
+add_action( 'wp_enqueue_scripts', 'for_the_future_scripts' );
 
 /**
  * first image
