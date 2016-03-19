1f7436c17b721ac4a0ace2b50f09babd2958a09f
travis-ci <wck2016@travis-ci.org>
2016-03-19 12:07:08 +0000

    Update from travis 09c1372ecf7bace2c1ab406e0b9efba34e52cb9f

--- singular.php
+++ singular.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-<div class="l-container">
 	<div class="singular-body">
 
 		<?php
@@ -15,23 +14,32 @@
 						</div>
 					<?php endif; ?>
 
-					<?php
-					the_title( '<h1 class="entry-title">', '</h1>' );
-					?>
+					<div class="l-container">
+						<?php
+						the_title( '<h1 class="entry-title">', '</h1>' );
+						?>
 
-				<?php if ( is_single() ) : ?>
-					<div class="entry-meta clearfix">
-						<time class="entry-date"><?php the_modified_date( 'Y/m/d' ); ?></time>
-						<p class="entry-category">
-							<i class="fa fa-folder-o" aria-label="category"></i><?php the_category(', '); ?>
-						</p>
-						<?php if ( has_tag() ) : ?>
-							<p class="entry-tag">
-								<i class="fa fa-tags" aria-label="tag"></i><?php the_tags(''); ?>
-							</p>
+						<?php if ( is_single() ) : ?>
+							<div class="entry-meta clearfix">
+								<time class="entry-date"><?php the_modified_date( 'Y/m/d' ); ?></time>
+								<p class="entry-category">
+									<i class="fa fa-folder-o" aria-label="category"></i><?php the_category(', '); ?>
+								</p>
+								<?php if ( has_tag() ) : ?>
+									<p class="entry-tag">
+										<i class="fa fa-tags" aria-label="tag"></i><?php the_tags(''); ?>
+									</p>
+								<?php endif; ?>
+							</div>
 						<?php endif; ?>
 					</div>
-				<?php endif; ?>
+				</header>
+
+				<div class="l-container">
+					<section class="entry-content">
+						<?php the_content(); ?>
+					</section>
+				</div>
 
 			</article>
 
