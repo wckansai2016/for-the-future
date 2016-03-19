569629cc9d305eb4c78e632d450c4f3112c84fb9
travis-ci <wck2016@travis-ci.org>
2016-03-19 09:37:49 +0000

    Update from travis e54c33d2ea9c5bef0cb1bbe08a23f5495c3478c4

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
 <?php get_header(); ?>
 
-<div class="gallery-container">
-<?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-<article <?php post_class(); ?>>
+<div class="l-container">
+	<div>
+		<div class="l-grid">
+			<?php if( have_posts() ): while( have_posts() ): the_post(); ?>
+				<div class="l-grid-col l-grid-col--1-4">
+					<article <?php post_class(); ?> style="overflow: hidden">
 
-	<div class="img-center">
-		<a href="<?php the_permalink(); ?>">
-		<?php
-			if( has_post_thumbnail() ) :
-				echo get_the_post_thumbnail( $post->ID, 'large' );
-			else :
-		?>
-		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/img_noimg.png" alt="<?php the_title(); ?>" style="width: 100%;" />
-		<?php
-			endif;
-		?>
-		</a>
-	</div>
-	<!--
+						<div class="img-center">
+							<a href="<?php the_permalink(); ?>">
+								<?php
+								if( has_post_thumbnail() ) :
+									echo get_the_post_thumbnail( $post->ID, 'large' );
+								else :
+									?>
+									<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/img_noimg.png" alt="<?php the_title(); ?>" style="width: 100%;" />
+									<?php
+								endif;
+								?>
+							</a>
+						</div>
+						<!--
 	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	-->
-	<!--
+						<!--
 	<?php the_content(); ?>
 	-->
-</article>
-<?php endwhile; endif; ?>
+					</article>
+				</div>
+
+			<?php endwhile; endif; ?>
+		</div>
+
+	</div>
 </div>
+
 
 <?php get_footer(); ?>
