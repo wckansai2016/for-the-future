eb28fbfad6d0cdde650a9ff79606abc6bf01a122
natashat <tokumotonahoko@gmail.com>
2016-03-19 17:02:19 +0900

    fix #78

--- singular.php
+++ singular.php
@@ -21,12 +21,12 @@
 				<?php if ( is_single() ) : ?>
 					<div class="entry-meta clearfix">
 						<time class="entry-date"><?php the_modified_date( 'Y/m/d' ); ?></time>
-						<dl class="entry-category">
+						<dl class="entry-category clearfix">
 							<dt><i class="fa fa-folder-o" aria-label="category"></i></dt>
 							<dd><?php the_category(', '); ?></dd>
 						</dl>
 						<?php if ( has_tag() ) : ?>
-							<dl class="entry-tag">
+							<dl class="entry-tag clearfix">
 								<dt><i class="fa fa-tags" aria-label="tag"></i></dt>
 								<dd><?php the_tags(''); ?></dd>
 							</dl>
