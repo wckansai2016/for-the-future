54fbbefece4f50ac616c7a61523c213f1968ab2a
hina <hina@hinaloe.net>
2016-03-20 22:27:23 +0900

    フッターナビを登録されている場合のみ表示

--- footer.php
+++ footer.php
@@ -2,12 +2,15 @@
 
 	<div class="l-container">
 		<h1 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1><!-- ロゴが登録されていたらロゴ画像、されてなければブログ名 -->
-		<?php wp_nav_menu( array(
-			'theme_location' => 'footer_nav',
-			'container' => '',
-			'menu_class' => '',
-			'depth' => 1,
-			'items_wrap' => '<ul class="sns-icon">%3$s</ul>'));
+		<?php if ( has_nav_menu( 'footer_nav' ) ) :
+			wp_nav_menu( array(
+				'theme_location' => 'footer_nav',
+				'container'      => '',
+				'menu_class'     => '',
+				'depth'          => 1,
+				'items_wrap'     => '<ul class="sns-icon">%3$s</ul>',
+			) );
+		endif;
 		?>
 
 		<p class="copyright">&copy;<?php echo date('Y '); bloginfo('name'); ?> All rights reserved.</p>
