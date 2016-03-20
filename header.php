ea7f044f32ca0947a7937eaf799b79685f3b51cc
travis-ci <wck2016@travis-ci.org>
2016-03-20 09:31:20 +0000

    Update from travis ea9508461e2b7311821740637d41a85637b09667

--- header.php
+++ header.php
@@ -1,36 +1,38 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<meta name="description" content="<?php bloginfo('description'); ?>" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
 
-	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/font-awesome-4.5.0/css/font-awesome.min.css" />
-	<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700|Noto+Serif:400,700' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/earlyaccess/notosansjapanese.css' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet"
+	      href="<?php echo esc_url( get_template_directory_uri() ); ?>/font-awesome-4.5.0/css/font-awesome.min.css"/>
+	<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700|Noto+Serif:400,700' rel='stylesheet'
+	      type='text/css'/>
+	<link href='//fonts.googleapis.com/earlyaccess/notosansjapanese.css' rel='stylesheet' type='text/css'/>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div class="l-container">
-		<header class="site-head">
-			<h1 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1><!-- ロゴが登録されていたらロゴ画像、されてなければブログ名 -->
-			<button type="button" class="nav-btn">
-				<span></span>
-				<span></span>
-				<span></span>
-				<span class="menu">MENU</span>
-			</button>
-			<nav class="gnav">
-				<?php get_search_form(); ?>
-				<?php wp_nav_menu( array(
-					'theme_location' => 'global_nav',
-					'container' => '',
-					'menu_class' => '',
-					'depth' => 3,
-					'items_wrap' => '<ul>%3$s</ul>'));
-				?>
-			</nav>
-		</header>
-
-	</div>
+<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
+	<header class="site-head">
+		<h1 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<!-- @todo: ロゴが登録されていたらロゴ画像、されてなければブログ名 -->
+		<button type="button" class="nav-btn">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span class="menu">MENU</span>
+		</button>
+		<nav class="gnav">
+			<?php get_search_form(); ?>
+			<?php wp_nav_menu( array(
+				'theme_location' => 'global_nav',
+				'container'      => '',
+				'menu_class'     => '',
+				'depth'          => 3,
+				'items_wrap'     => '<ul>%3$s</ul>'
+			) );
+			?>
+		</nav>
+	</header>
