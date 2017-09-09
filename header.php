0253438285c647104d8c096b66b41ed59a95fb1a
yat8823jp <yat@yat-net.com>
2017-09-10 01:44:59 +0900

    #325

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
 	<?php wp_head(); ?>
